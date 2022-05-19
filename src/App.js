5087b4cd0fed2885372be98cb36a2b1a5d06cd52
hh <2408138772@qq.com>
2022-05-19 22:09:53 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
 function App() {
+  let [person, setPerson] = useState("");
+  const [comment, setComment] = useState("");
+  let [commentList] = useState([]);
+  let [str, setStr] = useState("暂无评论，快去评论吧");
+  function addText() {
+    commentList.push({
+      person: person,
+      comment: comment,
+    });
+    let string = "";
+     string = commentList.map((item) => {
+      return `<p>评论人: ${item.person}</p>
+                <p>评论内容：${item.comment}</p>
+        `;
+    });
+    setStr(string);
+  }
+  function inputPerson(e) {
+    setPerson(e.target.value);
+  }
+  function inputComment(e) {
+    setComment(e.target.value);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainApp">
+      <input className="topInput" placeholder="请输入评论人" value={person} onChange={inputPerson} />
+      <br />
+      <textarea className="myTextarea" rows="18" placeholder="请输入评论内容" value={comment} onChange={inputComment} />
+      <button onClick={addText} className="btn">
+        发表评论
+      </button>
+      <div className="text" dangerouslySetInnerHTML={{ __html: str }}></div>
     </div>
   );
 }
-
 export default App;
